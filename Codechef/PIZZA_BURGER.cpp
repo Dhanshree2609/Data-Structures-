2a03485873e470a7d5f0157c332f2b8bca43eaf1
Dhanshree Handge <115164271+Dhanshree2609@users.noreply.github.com>
2022-10-19 10:45:52 +0530

    Create PIZZA_BURGER.cpp

---
+++ Codechef/PIZZA_BURGER.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+// your code goes here
+int t;
+cin>>t;
+while(t--)
+{
+   int x,y,z;
+   cin>>x>>y>>z;
+   
+   if(y<=x)
+   {
+       cout<<"PIZZA"<<endl;
+   }
+   else if(z<=x)
+   {
+       cout<<"BURGER"<<endl;
+   }
+   else if(x<y && x<z)
+   {
+       cout<<"NOTHING"<<endl;
+   }
+}
+return 0;
+}
